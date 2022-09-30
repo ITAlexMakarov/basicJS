f8cf631fe7116b704be17fd6315b93dd8cfd6ad8
Aleks Makarov <it.aleksmakarov@gmail.com>
2022-09-30 12:17:18 +0300

    HW array added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
+//=====task1=====//
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = arr1.every((i,pos) => arr1[i,pos] === arr2[i,pos]) && arr2.every((i,pos) => arr1[i,pos] === arr2[i,pos]); /*сравнение элементов
+  и их позиций массива1 и массива2, используя метод every, выступающей функцией высшего порядка по отношению к compareArrays()*/
   return result; // boolean
 }
 
+//=====task2=====//
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(i => i > 0).filter(i => i % 3 === 0).map(i => i * 10); /*положительные числа, кратные 3 и возвращюащие 
+  новый массив с отобранными числами, умноженными на 10, используя метод перебора массива filter и метод его преобразования map*/
+  
   return resultArr; // array
 }
