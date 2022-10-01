b37663980caa321df323218863a680db9168c33c
Aleks Makarov <it.aleksmakarov@gmail.com>
2022-10-01 14:25:57 +0300

    HW object added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
+new Student("Tony", "male", 37);
+new Student("Buzz", "female", 35);
+
+//2. Устанавка поля предмет `subject` экземпляра в `subjectName`
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+//3. Добавление студенту оценку `mark` в свойство (массив) `marks` объекта
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) { 
+    this.marks = [mark];  // добавить первую оценку 
+    } else {
+      this.marks.push(mark);   // добавить вторую и последующие оценки в массив
+    }
+}
+
+//4. Добавлять студенту сразу несколько оценок
+Student.prototype.addMarks = function (...mark) {
+  this.marks = mark;
+}
+
+//5. Среднее арифметическое оценок студента
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  let allAverage = sum / this.marks.length;
+  return allAverage;
+}
+
+//6. Исключение студента из учебного процесса и устанавливение причины исключения, а также удаление свойств subject и marks
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
